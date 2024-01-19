1922238b6f4fb019d985885e69e4f1f9ab00014c
NMcKittie <nathankit66@gmail.com>
2024-01-19 23:57:11 +0000

    Updated the function check_sequence(), not working

--- Winning_Clauses.py
+++ Winning_Clauses.py
@@ -12,22 +12,48 @@ def symbol_to_number(symbol):
         return int(symbol)
 
 
+# Checks if the hand is a straight by checking if the numbers are in sequence
 def check_sequence(a_dict):
+    a_dict = a_dict[0]
     print(a_dict)
     mykeys = list(a_dict.keys())
 
     mykeys.sort()
 
-    count = 0
+    
     first_val = mykeys[0]
-    for a in mykeys:
-        if first_val + count == a:
-            count += 1
-        else:
-            return False
-    return True
+    i = 0
+    high_seq = [0,0,0,0,0]
+
+    while i < len(mykeys) - 4:
+        temp_list = []
+        first_val = mykeys[i]
+        print("First_val " + str(first_val))
+        #print(first_val)
+       # first_val = first_val[0]
+        count = 0
+        k = i
+        while True:
+            num = mykeys[k]
+            print("index " + str(num))
+            if first_val + count == num:
+                temp_list.append(num)
+                count += 1
+                if count == 5: 
+                    break
+                k += 1
+            else:
+                temp_list = [0,0,0,0,0]
+                break
+        i += 1
+        #print(count)
+        if max(high_seq) < max(temp_list):
+            high_seq = temp_list
+    
+    return high_seq
 
 
+# Creates a tuple with 2 dictionaries inside, one counts the number of times a card value occurs. The other dict counts the number of times a suit occurs
 def count_cards(hand):
     number_dict = {}
     suit_dict = {}
@@ -50,6 +76,6 @@ def count_cards(hand):
 #def s(hand_ordered):
 
 
-tup = count_cards(["KD", "QL", "AL", "10J"])
+tup = count_cards(["2H", "3S", "4S", "5D", "6S", "2D", "8H", "9K", "10P", "11K", "12P"])
 print(tup)
-print(check_sequence(tup[0]))
+print(check_sequence(count_cards))
